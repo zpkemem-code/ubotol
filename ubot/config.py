515e710f99c2434721d3445b1de96edcb6403c26
zpcode <zpkemem@gmail.com>
2026-05-19 13:44:37 +0800

    Update config.py

--- ubot/config.py
+++ ubot/config.py
@@ -8,23 +8,23 @@ DEVS = [1838348402, 6051457085, 6696975845]
 
 
 
-API_ID = int(os.getenv("API_ID", "20304260"))
+API_ID = int(os.getenv("API_ID", "36794114"))
 
-API_HASH = os.getenv("API_HASH", "763e943987ee09ec449ad1611b7f5fc1")
+API_HASH = os.getenv("API_HASH", "dd9955a94f1fa9a7a7b39f461e638774")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7270284535:AAGh4Sxt9voXUgIZJIYHoAj4ixsVgK07H5s")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8960820741:AAHEiTNFFc0xhFoHSinl8HRA3IYeTbGNuiw")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "1838348402"))
+OWNER_ID = int(os.getenv("OWNER_ID", "8948668149"))
 
 USER_ID = list(map(int, os.getenv("USER_ID", "6696975845 1838348402 6696975845 6051457085 6527347171").split()))
 
-LOG_UBOT = int(os.getenv("LOG_UBOT", "-1002234875244"))
+LOG_UBOT = int(os.getenv("LOG_UBOT", "-1003992265929"))
 
-LOG_SELLER = int(os.getenv("LOG_SELLER", "-1002332295934"))
+LOG_SELLER = int(os.getenv("LOG_SELLER", "-1003992265929"))
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002075041230 -1002164901876 -1002166018703 -1001473548283 -1001390552926 -1001672802736 -1001917973794 -1002433894643").split()))
 
-MAX_BOT = int(os.getenv("MAX_BOT", "100"))
+MAX_BOT = int(os.getenv("MAX_BOT", "10"))
 
 RMBG_API = os.getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
 
@@ -34,4 +34,4 @@ OPENAI_KEY = os.getenv(
 ).split()
 
 MONGO_URL = os.getenv(
-    "MONGO_URL", "mongodb+srv://yxxxgans:bt30ztfR9EYPAnV0@botme.srpuzxu.mongodb.net/?retryWrites=true&w=majority&appName=botme")
+    "MONGO_URL", "mongodb+srv://biduan:biduan@cluster0.3uiosr7.mongodb.net/?appName=Cluster0")
